249ef2bb5dea2b1d6c621449b4c4aa20020c4dc4
Dino Maric <dino.onex@gmail.com>
2019-11-02 12:10:26 +0100

    Introduce Rails CurrentAttributes and Authentication concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Authentication
   include Pundit
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  private
 
- def user_not_authorized(exception)
-   flash[:error] = "Sorry, you aren't allowed to do that. You've been redirected to your previous page instead."
-   redirect_to(request.referrer || root_path)
- end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+    def user_not_authorized(exception)
+      flash[:error] = "Sorry, you aren't allowed to do that. You've been redirected to your previous page instead."
+      redirect_to(request.referrer || root_path)
+    end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,6 @@
 
 class Loan < ApplicationRecord
   belongs_to :carrier
+  belongs_to :lender, class_name: "User", default: -> { Current.user }
+  belongs_to :borrower, class_name: "User"
 end
